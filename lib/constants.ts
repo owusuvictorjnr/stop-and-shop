583a1af2c39ccdfd08352afd0eef0f5948a6148b
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-02 04:01:27 +0000

    constants

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Stop and Shop";
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Stop and Shop";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Start your day with the best deals";
+// export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
