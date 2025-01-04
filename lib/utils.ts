516f79da15aca878231e2f77b8e6f72fde1d57c6
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-04 11:19:07 +0000

    number formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatNumberWithDecimal = (num: number): string => {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : int;
+};
+
+export const toSlug = (text: string): string => {
+  return text
+    .toLowerCase() // Convert to lowercase
+    .replace(/[^\w\s-]/g, "") // Remove non-word, non-whitespace, and non-hyphen characters
+    .replace(/\s+/g, "-") // Replace whitespace with hyphens
+    .replace(/^-+|-+$/g, ""); // Trim leading and trailing hyphens
+};
